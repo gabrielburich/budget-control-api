4cf885a1bff5a48d1f52944f9c332d5ea1b324b4
gabrielburich <gabrielburich@gmail.com>
2019-12-02 20:25:07 -0300

    Atualizada porta do servidor para 3001

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,7 +1,7 @@
 import bodyParser from "body-parser";
 
 module.exports = server => {
-  server.set('port', 3000);
+  server.set('port', 3001);
   server.set('json spaces', 4);
   server.use(bodyParser.json());
   server.use((req, res, next) => {
